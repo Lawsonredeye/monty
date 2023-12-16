4bbdd7de61f93e75a259f42bf4d4633ee2aa46f4
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-16 22:58:50 +0100

    worked on the indentation and commenting style

--- opcodes.c
+++ opcodes.c
@@ -5,29 +5,30 @@
  * @stack: main stack
  * @line_number: line it was encountered
  * Return - nothing
-*/
+ */
 void pop(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp;
+	stack_t *temp;
 
-    if (*stack == NULL)
-        failure(line_number);
-    temp = *stack;
-    *stack = (*stack)->next;
-    /* check again if the stack is empty to assign prev to NULL*/
-    if (*stack != NULL)
-    {
-    (*stack)->prev = NULL;
-    free(temp);
-    }
+	if (*stack == NULL)
+		failure(line_number);
+	temp = *stack;
+	*stack = (*stack)->next;
+	/* check again if the stack is empty to assign prev to NULL*/
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+		free(temp);
+	}
 }
 
 /**
  * failure - prints the line error and terminates program
+ * @line_number: to indicate the current line of the problem
  * Return - nothing
-*/
+ */
 void failure(unsigned int line_number)
 {
-   fprintf(stderr, "L%d: usage: push integer\n", line_number);
+	fprintf(stderr, "L%d: usage: push integer\n", line_number);
 	exit(EXIT_FAILURE);
 }
