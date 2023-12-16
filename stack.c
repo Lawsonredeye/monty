d2b2732e5d5783f80ca06c5371bcc8ffec748d1f
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-16 23:02:55 +0100

    fixed indentation and fixed commenting style

--- stack.c
+++ stack.c
@@ -4,25 +4,25 @@
  * @stack: head node/link
  * @value: number being passed
  * Return: Nothing
-*/
+ */
 void push(stack_t **stack, unsigned int value)
 {
-    stack_t *temp;
+	stack_t *temp;
 
-    temp = malloc(sizeof(stack_t));
-    if (temp == NULL)
-    {
-        printf("malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-    temp->n = value;
-    temp->prev = NULL;
-    temp->next = *stack;
+	temp = malloc(sizeof(stack_t));
+	if (temp == NULL)
+	{
+		printf("malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	temp->n = value;
+	temp->prev = NULL;
+	temp->next = *stack;
 
-    if (*stack != NULL)
-        (*stack)->prev = temp;
-    
-    *stack = temp;
+	if (*stack != NULL)
+		(*stack)->prev = temp;
+
+	*stack = temp;
 }
 
 /**
@@ -30,35 +30,41 @@ void push(stack_t **stack, unsigned int value)
  * @stack: head node/link
  * @line_number: void
  * Return: Nothing
-*/
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
-    (void)line_number;
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	stack_t *current = *stack;
+	(void)line_number;
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
 
 /**
  * failed_prog - exit if the command passed is more than 2
- * 
  * Return - Nothing
-*/
-void failed_prog()
+ */
+void failed_prog(void)
 {
-    fprintf(stderr, "USAGE: monty file\n");
+	fprintf(stderr, "USAGE: monty file\n");
 	exit(EXIT_FAILURE);
 }
 
+/**
+ * pint - prints the value at the top of the
+ * stack, followed by a new line.
+ * @stack: head node to be used
+ * @line_number: current line number from program
+ * Return: Nothing (void)
+*/
 void pint(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL)
-    {
-        fprintf(stderr,"L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", (*stack)->n);
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
