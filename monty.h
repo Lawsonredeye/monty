778ba06cb1823f862e498e4f7dcf5239e734599a
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-17 16:38:36 +0100

    added void as the parameter to the prog func()

--- monty.h
+++ monty.h
@@ -42,6 +42,6 @@ void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
-void failed_prog();
+void failed_prog(void);
 void failure(unsigned int line_number);
 #endif
