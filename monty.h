fcc8f0f5312892e7490f36ebb2fb8407de24fd31
lawsonredeye <omoregbeeolawson@gmail.com>
2023-12-17 23:11:11 +0100

    updated this header file

--- monty.h
+++ monty.h
@@ -42,6 +42,7 @@ void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
 void failed_prog(void);
 void failure(unsigned int line_number);
 void free_all(stack_t **stack);
